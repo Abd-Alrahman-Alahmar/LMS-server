c46ae17449aed3046d34a18815b480fb29ed94a1
Abd Alrahman Alahmar <abdalrahman.alahmar@student.griffith.ie>
2024-01-10 23:10:39 +0000

    fourth deploy

--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://lms-client-snowy.vercel.app'],
     credentials: true,
 }));
 
